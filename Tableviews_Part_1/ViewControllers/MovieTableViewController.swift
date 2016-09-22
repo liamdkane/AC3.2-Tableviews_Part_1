4a49700f8765b5dc8f9b7feb0df7cc2727999537
Jason Gresh <jrgresh@gmail.com>
2016-09-22 11:42:44 -0400

    Step 1 finished

---
+++ Tableviews_Part_1/ViewControllers/MovieTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MovieTableViewController.swift
+//  Tableviews_Part_1
+//
+//  Created by Jason Gresh on 9/22/16.
+//  Copyright © 2016 C4Q. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Movies"
+        self.tableView.backgroundColor = UIColor.blue
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+
+
+
+
+
+
+}
